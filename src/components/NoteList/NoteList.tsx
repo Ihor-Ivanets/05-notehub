1c93b15cf3daaa0989185ea97439dffc96c725fd
Ihor Ivanets <ivanets22@gmail.com>
2026-01-08 00:49:26 +0200

    fixed errors

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,8 +1,4 @@
-import {
-  MutationCache,
-  useMutation,
-  useQueryClient,
-} from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import type { Note } from "../../types/note";
 import css from "./NoteList.module.css";
 import { deleteNote } from "../../services/noteService";
